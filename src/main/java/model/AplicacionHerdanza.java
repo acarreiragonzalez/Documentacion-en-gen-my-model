1566b204e55b55cb42002722d731185d5e66fad8
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2015-01-15 09:21:55 +0100

    Commit inicial

---
+++ src/main/java/model/AplicacionHerdanza.java
@@ -0,0 +1,36 @@
+package model;
+
+
+/**
+ * clase principal de donde se herda traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+
+public class AplicacionHerdanza
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public AplicacionHerdanza(){
+		super();
+	}
+
+	/**
+	 * metodo que calcula a clasificacion do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void amosarTipoTraballador() {
+		// TODO implement me	
+	}
+	
+}
+

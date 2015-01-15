84ed6ea058f9e948bc9b933c62dccbaf12083b1d
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2015-01-15 09:41:53 +0100

    Commit de parametros.

---
+++ src/main/java/model/Traballador.java
@@ -0,0 +1,111 @@
+package model;
+
+
+/**
+ * @author acarreiragonzalez
+ * Clase semiprincipal donde se clasifican os tipos de traballador.<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Traballador extends AplicacionHerdanza
+{
+	/**
+	 * nome do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String nome;
+	
+	/**
+	 * salario do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int soldo;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+         * @param nome
+         * @param soldo
+	 */
+	public Traballador(String nome,int soldo){
+             this.nome=nome;
+this.soldo=soldo;
+		
+	}
+
+	/**
+	 * metodo de acceso ao nome do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String getNome() {
+		// TODO implement me
+		return "";	
+	}
+	
+	/**
+	 * metodo que da valor ao nome do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setNome() {
+		// TODO implement me	
+	}
+	
+	/**
+	 * metodo de acceso ao soldo do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int getSoldo() {
+		// TODO implement me
+		return 0;	
+	}
+	
+	/**
+	 * metodo que da valor ao soldo do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setSoldo() {
+		// TODO implement me	
+	}
+	
+	/**
+	 * metodo que visualiza o nome e o soldo do traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String visualizarDatos() {
+		// TODO implement me
+		return "";	
+	}
+	
+}
+

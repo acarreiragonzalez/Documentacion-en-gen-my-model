0a2e687a114b5a46b28856247be4a90aeb6a04ba
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2015-01-15 09:30:54 +0100

    Commit de @author

---
+++ src/main/java/model/Directivo.java
@@ -0,0 +1,85 @@
+package model;
+
+
+/**
+ * @author acarreiragonzalez
+ * Clase donde son direccionados os traballadores tipo directivo. herda da clase traballador<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Directivo extends Traballador
+{
+	/**
+	 * Atributo de salarios<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int dietas;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Directivo(){
+		super();
+	}
+
+	/**
+	 * Metodo de acceso a dietas<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int getDietas() {
+		// TODO implement me
+		return 0;	
+	}
+	
+	/**
+	 * Metodo que da valor a dietas<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setDietas() {
+		// TODO implement me	
+	}
+	
+	/**
+	 * Metodo que calcula las dietas<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int calcularDietas() {
+		// TODO implement me
+		return 0;	
+	}
+	
+	/**
+	 * Metodo que calcular el sueldo<br><div style="display:none;" id="__if72ru4sdfsdfrkjahiuyi_once"></div><div style="display:none;" id="__hggasdgjhsagd_once"></div>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int calcularSoldo() {
+		// TODO implement me
+		return 0;	
+	}
+	
+}
+
